565b4903fd2306789a27335aacf731baf3d76a18
tmortagne <thomas.mortagne@gmail.com>
2013-09-09 14:52:53 +0200

    Add SSL support

--- NetworkStat/src/org/atelierdesmedias/presence/networkstat/NetworkStatService.java
+++ NetworkStat/src/org/atelierdesmedias/presence/networkstat/NetworkStatService.java
@@ -1,6 +1,8 @@
 package org.atelierdesmedias.presence.networkstat;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 import org.apache.http.HttpException;
@@ -8,6 +10,7 @@ import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.AuthState;
@@ -16,25 +19,69 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerPNames;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.ExecutionContext;
 import org.apache.http.protocol.HttpContext;
+import org.atelierdesmedias.presence.networkstat.ssl.EasySSLSocketFactory;
 
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
 public class NetworkStatService extends IntentService {
-	private HttpRequestInterceptor preemptiveAuth;
+	//private HttpRequestInterceptor preemptiveAuth;
+	private ClientConnectionManager clientConnectionManager;
+	private HttpParams httpParams;
+	private HttpContext httpContext;
 
-	public NetworkStatService() {
+	public NetworkStatService() throws NoSuchAlgorithmException,
+			KeyManagementException {
 		super(NetworkStatService.class.getName());
 
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+
+		// http scheme
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory
+				.getSocketFactory(), 80));
+		// https scheme
+		schemeRegistry.register(new Scheme("https", new EasySSLSocketFactory(),
+				443));
+
+		this.httpParams = new BasicHttpParams();
+		this.httpParams.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 1);
+		this.httpParams.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE,
+				new ConnPerRouteBean(1));
+		this.httpParams.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+		HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(httpParams, "utf8");
+
+		/*CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		// TODO: make credentials configurable
+		credentialsProvider.setCredentials(AuthScope.ANY,
+				new UsernamePasswordCredentials("Admin", "admin"));*/
+		this.clientConnectionManager = new ThreadSafeClientConnManager(httpParams,
+				schemeRegistry);
+
+		this.httpContext = new BasicHttpContext();
+		/*this.context.setAttribute("http.auth.credentials-provider",
+				credentialsProvider);*/
+
 		// Prepare preemptive filter
-		this.preemptiveAuth = new HttpRequestInterceptor() {
+		/*this.preemptiveAuth = new HttpRequestInterceptor() {
 			public void process(final HttpRequest request,
 					final HttpContext context) throws HttpException,
 					IOException {
@@ -55,7 +102,7 @@ public class NetworkStatService extends IntentService {
 					}
 				}
 			}
-		};
+		};*/
 	}
 
 	@Override
@@ -79,24 +126,25 @@ public class NetworkStatService extends IntentService {
 		// ////////////////////////
 
 		try {
-			CredentialsProvider credProvider = new BasicCredentialsProvider();
+			DefaultHttpClient adminClient = new DefaultHttpClient(this.clientConnectionManager, this.httpParams);
+
+			/*CredentialsProvider credProvider = new BasicCredentialsProvider();
 
 			// TODO: make credentials configurable
 			credProvider.setCredentials(AuthScope.ANY,
 					new UsernamePasswordCredentials("Admin", "admin"));
 
-			DefaultHttpClient adminClient = new DefaultHttpClient();
 			adminClient.setCredentialsProvider(credProvider);
-			adminClient.addRequestInterceptor(preemptiveAuth, 0);
+			adminClient.addRequestInterceptor(preemptiveAuth, 0);*/
 
 			// TODO: make the URL configurable
 			HttpPost put = new HttpPost(
-					"http://xwiki.atelier-medias.org/xwiki/bin/get/PresenceCode/StoreService?datetime="
+					"https://xwiki.atelier-medias.org/xwiki/bin/get/PresenceCode/StoreService?datetime="
 							+ new Date().getTime());
 
 			put.setEntity(new ByteArrayEntity(stats));
 
-			HttpResponse response = adminClient.execute(put);
+			HttpResponse response = adminClient.execute(put, this.httpContext);
 
 			StatusLine line = response.getStatusLine();
 			if (line.getStatusCode() != 200) {
